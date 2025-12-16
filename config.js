de8b4e0bde7a90addc1d8d4e8cd65c8c8fe8237a
Amel20011 <amelcns8@gmail.com>
2025-12-17 06:46:22 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,26 @@
+export const BOT = {
+  name: "Liviaa🌷",
+  ownerNumber: "13658700681", // +1 (365) 870-0681
+  prefix: ".",
+  mode: "public", // "public" | "self"
+  footer: "ᥫ᭡ ⋆˚꩜｡ Liviaa🌷 𓂃💗",
+  brand: "ᯓᡣ𐭩 ⋆.𐙚 ̊ ⋆˚꩜｡ᥫ᭡",
+  sessionDir: "./session",
+  media: {
+    menu: "./media/menu.jpg",
+    welcome: "./media/welcome.jpg",
+    qris: "./media/qris.jpg"
+  }
+};
+
+export const SERVER = {
+  host: "0.0.0.0",
+  port: 3000
+};
+
+export const UI = {
+  boldCute: (s) => `⋆˚꩜｡ ${s} 𓂃💗`,
+  title: (s) => `ᯓᡣ𐭩 ${s} ⋆.𐙚`,
+  bullet: "ᥫ᭡",
+  icons: ["🌷", "☘️", "💗", "🌹"]
+};
